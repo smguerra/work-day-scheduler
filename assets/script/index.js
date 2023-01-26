d769dff64cfbb46f9b4dc05bdd56a827409e6d84
Milena <milenagg77@hotmail.com>
2023-01-26 22:32:17 +0000

    added function displayTime

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,1 +1,12 @@
-alert("hola");
+// var timeDisplayEl = $('#currentDay');
+// // handle displaying the time
+
+var today = moment();
+
+function displayTime() {
+    var dayWeek = today.format("dddd, MMMM Do")
+    $("#currentDay").text(dayWeek);
+  };
+ 
+displayTime();
+
