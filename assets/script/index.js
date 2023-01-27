76f79a6db79daf7c98d4fa4e3bc7ff1d126b6fcf
Milena <milenagg77@hotmail.com>
2023-01-27 23:19:52 +0000

    added code to html for presenting timeblocks for standard business hours when the user scrolls down

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,14 +1,7 @@
-// var timeDisplayEl = $('#currentDay');
-// // handle displaying the time
-
 //Display the current day at the top of the calender when a user opens the planner.
 
-var today = moment();
+let currentDay = moment().format("dddd, MMMM Do");
+$("#currentDay").text(currentDay);
 
-function displayTime() {
-    var dayWeek = today.format("dddd, MMMM Do");
-    $("#currentDay").text(dayWeek);
-  };
- 
-displayTime();
+
 
--- assets/script/scirot.js
+++ assets/script/scirot.js
@@ -1,128 +1,53 @@
-<!DOCTYPE html>
-<html lang="en-gb">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link
-      rel="stylesheet"
-      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    />
-    <link
-      rel="stylesheet"
-      href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-      integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
-      crossorigin="anonymous"
-    />
-    <link
-      href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap"
-      rel="stylesheet"
-    />
-    <link
-      rel="stylesheet"
-      href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"
-    />
-    <link rel="stylesheet" href="./assets/css/style.css" />
-    <title>Work Day Scheduler</title>
-  </head>
-
-  <body>
-    <header class="jumbotron">
-      <h1 class="display-3">Work Day Scheduler</h1>
-      <p class="lead">A simple calendar app for scheduling your work day</p>
-      <p id="currentDay" class="lead"></p>
-    </header>
-    <div class="container">
-      <!-- Timeblocks go here -->
-    </div>
-    
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <script src="./assets/script/index.js"></script>
-  </body>
-</html>
-
-body {
-    font-family: 'Open Sans', sans-serif;
-    font-size: 16px;
-    line-height: 1;
+// create timeblocks for standard business hours (9am - 5pm)
+for (let i = 9; i < 17; i++) {
+    // create a row for each hour
+    let row = $("<div>").addClass("row time-block");
+    // add hour label
+    let hour = $("<div>")
+      .addClass("col-2 hour")
+      .text(`${i}:00`);
+    // add event input field
+    let event = $("<textarea>").addClass("col-8 description");
+    // add save button
+    let save = $("<button>")
+      .addClass("col-2 saveBtn")
+      .html("<i class='fas fa-save'></i>");
+  
+    // append elements to row
+    row.append(hour, event, save);
+    // append row to container
+    $(".container").append(row);
+  
+    // check if current time is past, present, or future
+    let currentTime = moment().hours();
+    if (i < currentTime) {
+      row.addClass("past");
+    } else if (i === currentTime) {
+      row.addClass("present");
+    } else {
+      row.addClass("future");
+    }
   }
   
-  textarea {
-    background: transparent;
-    border: none;
-    resize: none;
-    color: #000000;
-    border-left: 1px solid black;
-    padding: 10px;
-  }
+  // save event to local storage when save button is clicked
+  $(".saveBtn").on("click", function() {
+    let event = $(this)
+      .siblings("textarea")
+      .val();
+    let hour = $(this)
+      .siblings(".hour")
+      .text();
+    localStorage.setItem(hour, event);
+  });
   
-  .jumbotron {
-    text-align: center;
-    background-color: transparent;
-    color: black;
-    border-radius: 0;
-    border-bottom: 10px solid black;
-  }
+  // retrieve events from local storage and display in textarea
+  $(".description").each(function() {
+    let hour = $(this)
+      .siblings(".hour")
+      .text();
+    let event = localStorage.getItem(hour);
+    $(this).val(event);
+  });
   
-  .description {
-    white-space: pre-wrap;
-  }
-  
-  .time-block {
-    text-align: center;
-    border-radius: 15px;
-  }
-  
-  .row {
-    white-space: pre-wrap;
-    height: 80px;
-    border-top: 1px solid white;
-  }
-  
-  .hour {
-    background-color: #ffffff;
-    color: #000000;
-    border-top: 1px dashed #000000;
-  }
-  
-  .past {
-    background-color: #d3d3d3;
-    color: white;
-  }
-  
-  .present {
-    background-color: #ff6961;
-    color: white;
-  }
-  
-  .future {
-    background-color: #77dd77;
-    color: white;
-  }
-  
-  .saveBtn {
-    border-left: 1px solid black;
-    border-top-right-radius: 15px;
-    border-bottom-right-radius: 15px;
-    background-color: #06aed5;
-    color: white;
-  }
-  
-  .saveBtn i:hover {
-    font-size: 20px;
-    transition: all 0.3s ease-in-out;
-  }
-
-  
-  var today = moment();
-
-  function displayTime() {
-      var dayWeek = today.format("dddd, MMMM Do")
-      $("#currentDay").text(dayWeek);
-    };
-   
-  displayTime();
   
   
