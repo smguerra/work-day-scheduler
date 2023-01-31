c5ee7ac43ce409a083352d78385f65b0e4e8cc3c
Milena <milenagg77@hotmail.com>
2023-01-31 12:44:32 +0000

    created code to save the event in local storage when the save button is clicked in that timeblock.

--- assets/script/index.js
+++ assets/script/index.js
@@ -6,21 +6,24 @@ $("#currentDay").text(currentDay);
 
 for (let i = 9; i < 18; i++) {
 
+
 //create a row for each hour and add classes
 
 let row = $("<div>").addClass("row time-block");
+$(row).attr("id","hour-" + [i]);
 
 //create a hour div to show hour from 9 am and add classes
 
-let hour = $("<div>").addClass("col-1 hour");
+let hour = $("<div>").addClass("col-1 hour hour-" + [i]).text([i]);
+
 
 // create a textarea element for data input and add classes
 
 let input = $("<textarea>").addClass("col-10 description");
 
-//create a button element to save input from user and add classes
+//create a button element to save input from user and add classes and icon
 
-let button = $("<button>").addClass("col-1 saveBtn");
+let button = $("<button>").addClass("col-1 btn saveBtn fas fa-save");
 
 //append hour, input and button to the whole row 
 
@@ -30,28 +33,52 @@ row.append(hour, input, button);
 
 container.append(row);
 
-let currentHour = moment().hour();
+let currentHour = moment().hours();
 
-console.log(currentHour);
+//console.log(currentHour);
+
+//add classes of past, present and future to the row div
 
 if (i < currentHour) {
+
     row.addClass("past");
+
 } else if (i === currentHour) {
+
     row.addClass("present");
+
 } else {
+
     row.addClass("future");
 };
 
-}
-// function currentHour () {
-//     let row = $(".row");
+};
 
-//     for (let i = 9; i < 18; i++) {
-//     };
-//     return currentHour();
-// };
+//Save the event in local storage when the save button is clicked in that timeblock.
+
+$(".saveBtn").on("click", function () {
+    let saveInput = $(this).siblings("textarea").val();
+    //let saveInput = $("textarea").val();
+    let time = $(this).parent().attr("id")
+    localStorage.setItem(time, saveInput);
+});
+
+// //Persist events between refreshes of a page
+// //retrieve the value stored in the browser's local storage with the key "hour-9" 
+// //and sets it as the value of the textarea inside the element with an id of "hour-9"
+
+// $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+// $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+// $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+// $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+// $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+// $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+// $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+// $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+// $('#hour-17 .description').val(localStorage.getItem('hour-17'));
 
 
+  
 
 
 
--- assets/script/scirot.js
+++ assets/script/scirot.js
@@ -1,53 +1,52 @@
-// create timeblocks for standard business hours (9am - 5pm)
-for (let i = 9; i < 17; i++) {
-    // create a row for each hour
-    let row = $("<div>").addClass("row time-block");
-    // add hour label
-    let hour = $("<div>")
-      .addClass("col-2 hour")
-      .text(`${i}:00`);
-    // add event input field
-    let event = $("<textarea>").addClass("col-8 description");
-    // add save button
-    let save = $("<button>")
-      .addClass("col-2 saveBtn")
-      .html("<i class='fas fa-save'></i>");
+
   
-    // append elements to row
-    row.append(hour, event, save);
-    // append row to container
-    $(".container").append(row);
-  
-    // check if current time is past, present, or future
-    let currentTime = moment().hours();
-    if (i < currentTime) {
-      row.addClass("past");
-    } else if (i === currentTime) {
-      row.addClass("present");
-    } else {
-      row.addClass("future");
-    }
-  }
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   // save event to local storage when save button is clicked
   $(".saveBtn").on("click", function() {
-    let event = $(this)
-      .siblings("textarea")
-      .val();
-    let hour = $(this)
-      .siblings(".hour")
-      .text();
+    let event = $(this).siblings("textarea").val();
+    let hour = $(this).siblings(".hour").text();
     localStorage.setItem(hour, event);
   });
   
+  $(".saveBtn").on("click", function () {
+    let saveInput = $("textarea").val();
+    localStorage.setItem("text", saveInput);
+
+})
   // retrieve events from local storage and display in textarea
   $(".description").each(function() {
-    let hour = $(this)
-      .siblings(".hour")
-      .text();
+    let hour = $(this).siblings(".hour").text();
     let event = localStorage.getItem(hour);
     $(this).val(event);
   });
   
-  
+
   
