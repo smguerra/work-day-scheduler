52dc2366039128f1244daa635d748de38b4d0504
Milena <milenagg77@hotmail.com>
2023-01-31 12:58:41 +0000

    created code to show that events persist between refreshes of a page

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # work-day-scheduler
+
+
 Work Day Scheduler
+
+
+## Description 
+
+
+This JavaScript challenge is a simple calendar application that allows a user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery. It showcases my skills and talents in the development of JavaScript code using JQuery and Moment.js library. 
+
+It displays the current day at the top of the calender when a user opens the planner. Then, it will present time-blocks for standard business hours when the user scrolls down. It also provides a color-code feature for each time-block based on past, present, and future when the time-block is viewed. Finally, it allow a user to enter an event when they click a time-block and save the event in the local storage when the save button is clicked in that time-block, this information will persist between refreshes of a page.
+
+
+## Screenshot
+
+
+
+
+
+
+## Tech Stack
+
+
+- JavaScript 
+- HTML
+- CSS
+- jQuery
+- Moment.js library
+
+
+## Links
+
+
+- Project URL:  https://github.com/smguerra/Console-Finances
+- Live Site URL:  https://smguerra.github.io/Password-Generator/
+
+
+## Author
+
+Sandra Guerra
+
+
+## Copyright
+
+© 2022 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
--- assets/script/index.js
+++ assets/script/index.js
@@ -63,19 +63,20 @@ $(".saveBtn").on("click", function () {
     localStorage.setItem(time, saveInput);
 });
 
-// //Persist events between refreshes of a page
-// //retrieve the value stored in the browser's local storage with the key "hour-9" 
-// //and sets it as the value of the textarea inside the element with an id of "hour-9"
+//Persist events between refreshes of a page
+//retrieve the value stored in the browser's local storage with the key "hour-9" 
+//and sets it as the value of the textarea inside the element with an id of "hour-9"
 
-// $('#hour-9 .description').val(localStorage.getItem('hour-9'));
-// $('#hour-10 .description').val(localStorage.getItem('hour-10'));
-// $('#hour-11 .description').val(localStorage.getItem('hour-11'));
-// $('#hour-12 .description').val(localStorage.getItem('hour-12'));
-// $('#hour-13 .description').val(localStorage.getItem('hour-13'));
-// $('#hour-14 .description').val(localStorage.getItem('hour-14'));
-// $('#hour-15 .description').val(localStorage.getItem('hour-15'));
-// $('#hour-16 .description').val(localStorage.getItem('hour-16'));
-// $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+
+$('#hour-9 .description').val(localStorage.getItem('hour-9'));
+$('#hour-10 .description').val(localStorage.getItem('hour-10'));
+$('#hour-11 .description').val(localStorage.getItem('hour-11'));
+$('#hour-12 .description').val(localStorage.getItem('hour-12'));
+$('#hour-13 .description').val(localStorage.getItem('hour-13'));
+$('#hour-14 .description').val(localStorage.getItem('hour-14'));
+$('#hour-15 .description').val(localStorage.getItem('hour-15'));
+$('#hour-16 .description').val(localStorage.getItem('hour-16'));
+$('#hour-17 .description').val(localStorage.getItem('hour-17'));
 
 
   
